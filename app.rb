1374cb8cf1cedbf39b823715334d60e32e1ab1a8
Andrew Kreps <andrew.kreps@gmail.com>
2013-07-30 23:23:14 -0700

    Refactor of extra-class code into the class and centralizing the folder list.

--- app.rb
+++ app.rb
@@ -6,23 +6,25 @@ require 'dm-postgres-adapter'
 DataMapper::Logger.new($stdout, :debug)
 DataMapper::Property::String.length(4000)
 #DataMapper.setup(:default, "sqlite://#{File.expand_path(File.dirname(__FILE__))}/sample.sqlite")
-DataMapper.setup(:default, "postgres://localhost/database")
-
-["models", "helpers", "controllers", "routes"].each do |folder|
-	Dir.glob("#{folder}/*.rb").each { |file| require_relative file }
-end
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
-# DataMapper.auto_migrate!  # This one wipes the database out every time.  Good for testing.
+DataMapper.setup(:default, "postgres://localhost/square-peg")
 
 class App < Sinatra::Base
 	helpers Sinatra::Helpers
 
+	app_folders = ["models", "helpers", "controllers", "routes"]
+	# Require our ruby fileses
+	app_folders.each do |folder|
+		Dir.glob("#{folder}/*.rb").each { |file| require_relative file }
+	end
+
+	DataMapper.finalize
+	DataMapper.auto_upgrade!
+	# DataMapper.auto_migrate!  # This one wipes the database out every time.  Good for testing.
+
 	configure :development do
 		require 'sinatra/reloader'
 		register Sinatra::Reloader
-		["models", "helpers", "controllers", "routes"].each do |folder|
+		app_folders.each do |folder|
 			Dir.glob("#{folder}/*.rb").each { |file| also_reload file }
 		end
 	end
